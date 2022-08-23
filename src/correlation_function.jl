d02ed2d86485b644f1fb7cd6d4380b52e7704ae9
John Tipton <jtipton25@gmail.com>
2022-08-23 13:50:45 -0500

    resolving matern at 0 issue

--- src/correlation_function.jl
+++ src/correlation_function.jl
@@ -10,8 +10,9 @@ Return an isotropic and stationary Matern covariance matrix with variance `tau2`
 """
 function matern(d, nu, phi)
     dd = d / phi
-    if isapprox(dd, 0.0, atol=1e-10)
-        # 1.0
+    if dd == 0.0
+        return(1.0)
+    elseif isapprox(dd, 0.0, atol=1e-10)
         dd = 1e-10
     end
     return 1.0 / (2.0^(nu - 1.0) * gamma(nu)) * (dd^nu) * besselk(nu, dd)
